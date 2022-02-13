1c6501551ece33f7e3ae96bbf6f6be7da12b55f2
VrushaketPatwardhan <59718916+VrushaketPatwardhan@users.noreply.github.com>
2022-02-13 21:07:55 +0530

    Updated Assignment-2 and added commments

--- Assignment-2.py
+++ Assignment-2.py
@@ -1,31 +1,38 @@
-import math                                
+#BT19ECE034
+#Assignment 2:
+# 1) Read and dispaly a color image
+# 2) Convert color image to grayscale
+# 3) Making some part of the image black
+
+# Importing required libraries
+import math                                               
 import numpy as np
 import cv2
 
-I1 = np.zeros((512,512),np.uint8)        
-I2 = np.zeros((512,512),np.uint8)
+path = r"C:\Users\USER\Desktop\wallpapers\vrushaket.jpeg" #declaring path of image
+image = cv2.imread(path)                                  #Reading the image   
+m,n,v = image.shape                                      
+gray_image = np.zeros((m,n),np.uint8)                   
+result_image = np.zeros((m,n),np.uint8)  
 
-I1 = cv2.circle(I1,(235,250),80,(255,255),-1)            
-I2 = cv2.rectangle(I2,(100,300),(330,190),(255,255,255),-1)          
+for i in range(m):              #Converting color Image to Grayscale by averaging
+    for j in range(n): 
+        k=0;                           
+        for k in range(v):
+            k += math.floor(image[i][j][k]/3)
 
+        gray_image[i][j] = k
+        result_image[i][j] = k
 
-cv2.imshow("Image with white rectangle at center",I2)
-cv2.waitKey(0)
-cv2.imshow("Image with white circle at center",I1)
-cv2.waitKey(0)
+for x in range(35,270,1):         #Making some part of the image black
+   for y in range(45,270,1):
+       result_image[x][y] = 0
 
-
-cv2.imshow("AND operation on images",cv2.bitwise_and(I1,I2))
+#Dispalying the Images
+cv2.imshow("original Image",image)
 cv2.waitKey(0)
-cv2.imshow("OR operation on images",cv2.bitwise_or(I1,I2))
+cv2.imshow('Grayscale Image',gray_image)
 cv2.waitKey(0)
-
-cv2.imshow("NAND operation on images",cv2.bitwise_not(cv2.bitwise_and(I1,I2)))
+cv2.imshow('New Image',result_image)
 cv2.waitKey(0)
-cv2.imshow("NOR operation on images",cv2.bitwise_not(cv2.bitwise_or(I1,I2)))
-cv2.waitKey(0)
-
-cv2.imshow("EXOR operation on images",cv2.bitwise_xor(I1,I2))
-cv2.waitKey(0)
-cv2.imshow("EXNOR operation on images",cv2.bitwise_not(cv2.xor(I1,I2)))
-cv2.waitKey(0)
+cv2.destroyAllWindows()
